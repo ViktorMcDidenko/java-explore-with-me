fd0d5b97d1081157ae320aa9bd7badc6e69c09a7
Viktor Didenko <viktor_didenko@inbox.ru>
2023-07-31 14:04:18 +0300

    fix: findByIdIn user query

--- main-service/src/main/java/ru/practicum/ewm/repository/UserRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/UserRepository.java
@@ -3,17 +3,14 @@ package ru.practicum.ewm.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.model.User;
 
 import java.util.List;
 import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "SELECT * " +
-            "FROM users u " +
-            "WHERE (:ids IS NULL OR u.id IN :ids)", nativeQuery = true)
-    List<User> findByIdIn(@Param("ids") Set<Long> ids, Pageable pageable);
+    @Query(value = "select u from User u where (:ids is null or u.id in :ids)")
+    List<User> findByIdIn(Set<Long> ids, Pageable pageable);
 
     boolean existsByName(String name);
 }
